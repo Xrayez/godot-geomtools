a0d2251e2576c459d06d160f0a13210a44166af3
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-05-16 20:07:54 +0300

    Implement polygon convex partitioning

--- 2d/polytools/decomp/poly_decomp.cpp
+++ 2d/polytools/decomp/poly_decomp.cpp
@@ -1,5 +1,7 @@
+#include "modules/geomtools/2d/geometry_tools.h"
 #include "poly_decomp.h"
 #include "core/math/triangulate.h"
+#include "thirdparty/misc/triangulator.h"
 
 Vector<Vector<Point2> > PolyDecompBase2D::decompose_polygons(DecompType p_type, const Vector<Vector<Point2> > &p_polygons) {
 	Vector<Vector<Point2> > ret;
@@ -37,7 +39,55 @@ Vector<Vector<Point2> > PolyDecompBase2D::triangulate_polygons(const Vector<Vect
 }
 
 Vector<Vector<Point2> > PolyDecompBase2D::decompose_polygons_into_convex(const Vector<Vector<Point2> > &p_polygons) {
-    Vector<Vector<Point2> > convex;
+	Vector<Vector<Point2> > convex;
+	List<TriangulatorPoly> in_poly, out_poly;
+    
+    // Split between inner and outer polygons first.
+    Vector<int> outer_indices, inner_indices;
+    for (int i = 0; i < p_polygons.size(); ++i) {
+        if (GeometryTools2D::polygon_area(p_polygons[i]) >= 0.0) {
+            outer_indices.push_back(i);
+        } else {
+            inner_indices.push_back(i);
+        }
+    }
+    // Setup outer polygons.
+    for (int i = 0; i < outer_indices.size(); ++i) {
+        TriangulatorPoly inp_outer;
+        const Vector<Point2> &polygon_outer = p_polygons[outer_indices[i]];
+        inp_outer.Init(polygon_outer.size());
+        for (int i = 0; i < polygon_outer.size(); i++) {
+            inp_outer.GetPoint(i) = polygon_outer[i];
+        }
+        in_poly.push_back(inp_outer);
+    }
+    // Setup inner polygons.
+    for (int i = 0; i < inner_indices.size(); ++i) {
+        TriangulatorPoly inp_inner;
+        const Vector<Point2> &polygon_inner = p_polygons[inner_indices[i]];
+        inp_inner.Init(polygon_inner.size());
+        for (int j = 0; j < polygon_inner.size(); ++j) {
+            inp_inner.GetPoint(j) = polygon_inner[j];
+        }
+        inp_inner.SetHole(true);
+        in_poly.push_back(inp_inner);
+    }
+    // Decompose!
+	TriangulatorPartition tpart;
+	if (tpart.ConvexPartition_HM(&in_poly, &out_poly) == 0) {
+		ERR_PRINT("Convex decomposition failed!");
+		return convex;
+	}
+	convex.resize(out_poly.size());
+	int idx = 0;
+	for (List<TriangulatorPoly>::Element *I = out_poly.front(); I; I = I->next()) {
+		TriangulatorPoly &tp = I->get();
+		convex.write[idx].resize(tp.GetNumPoints());
+		for (int64_t i = 0; i < tp.GetNumPoints(); i++) {
+			convex.write[idx].write[i] = tp.GetPoint(i);
+		}
+		idx++;
+	}
     return convex;
 }
 
